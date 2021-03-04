fd57728ea18838b225d976140b2e4412ac98ef9d
LuLu GiGi <lucamaggio98@hotmail.com>
2021-03-04 14:32:49 +0100

    wip: creating PlayerController with template

--- Backend-Api/runFlaskApp.py
+++ Backend-Api/runFlaskApp.py
@@ -2,5 +2,6 @@ from FlaskApp import NavalCrudApp
 from Controllers import TestController
 from Controllers import GridController
 from Controllers import LobbyController
+from Controllers import PlayerController
 
 NavalCrudApp.run(debug=True, port=5001)
