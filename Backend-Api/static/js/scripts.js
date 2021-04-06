40decb3463ee0a3251558a64438f20f48a2800e8
LuLu GiGi <lucamaggio98@hotmail.com>
2021-04-06 14:25:59 +0200

    wip: add a function to the clickevent of every cell

--- Backend-Api/static/js/scripts.js
+++ Backend-Api/static/js/scripts.js
@@ -22,14 +22,20 @@ const container = document.getElementById("gridContainer");
     row.style.display = "flex";
     row.style.flexDirection = "row";
         for (y = 0; y < gridY; y++) {
+            var id = "X"+x+"Y"+y;
             let cell = document.createElement("div");
-            cell.setAttribute("id", "X"+x+"Y"+y);
+            cell.setAttribute("id", id);
             cell.style.padding = "10px";
             cell.style.border = "1px solid";
+            cell.addEventListener("click", function(){onCellClick(cell);});
             row.appendChild(cell);
         }
     container.appendChild(row);
     }
+}
+
+function onCellClick(cell){
+    console.log(`cell ${cell.id} clicked`);
 }
 
 function placeBoat(){
