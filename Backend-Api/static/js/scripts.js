c60f96873d63236a3188bdef01ea1ae6d0c521c3
LuLu GiGi <lucamaggio98@hotmail.com>
2021-04-06 12:00:57 +0200

    wip: creating grid with javascript, onload of body call script function

--- Backend-Api/static/js/scripts.js
+++ Backend-Api/static/js/scripts.js
@@ -13,3 +13,25 @@ function createLobby(player){
 //    window.location.href = "http://www.w3schools.com";
 //    let myFetch = fetch(url);
 }
+
+function createGrid(gridX, gridY){
+const container = document.getElementById("gridContainer");
+
+    for (x = 0; x < gridX; x++) {
+    let row = document.createElement("div");
+    row.style.display = "flex";
+    row.style.flexDirection = "row";
+        for (y = 0; y < gridY; y++) {
+            let cell = document.createElement("div");
+            cell.setAttribute("id", "X"+x+"Y"+y);
+            cell.style.padding = "10px";
+            cell.style.border = "1px solid";
+            row.appendChild(cell);
+        }
+    container.appendChild(row);
+    }
+}
+
+function placeBoat(){
+    console.log("10");
+}
