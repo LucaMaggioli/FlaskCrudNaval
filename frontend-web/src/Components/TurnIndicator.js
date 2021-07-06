0d7656acdbe983644d07ce65a1d047539bc77746
LuLu GiGi <lucamaggio98@hotmail.com>
2021-07-06 14:13:57 +0200

    ref: adding files

---
+++ frontend-web/src/Components/TurnIndicator.js
@@ -0,0 +1,27 @@
+import { Box } from "@material-ui/core";
+import { useNavalBattleContext } from "../hooks/NavalBattleContextProvider";
+import { GameStatuses } from "./Constants";
+
+export default function TurnIndicator() {
+  const { gameState } = useNavalBattleContext();
+
+  let text = "text";
+  switch (gameState) {
+    case GameStatuses.PLAYER1TURN:
+      text = "It's player 1 turn !";
+      break;
+    case GameStatuses.PLAYER2TURN:
+      text = "It's player 2 turn !";
+      break;
+    default:
+    // code block in case
+  }
+
+  return (
+    <Box>
+      <h1>Player turn indicator ^.^</h1>
+      <h2>{gameState}</h2>
+      <h2>{text}</h2>
+    </Box>
+  );
+}
