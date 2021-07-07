10fcb7f71e556af6f3864f5d8843c307b3c808b1
LuLu GiGi <lucamaggio98@hotmail.com>
2021-07-07 14:19:23 +0200

    feat: component to display who is the winner

---
+++ frontend-web/src/Components/FinishView.js
@@ -0,0 +1,57 @@
+import { useNavalBattleContext } from "../hooks/NavalBattleContextProvider";
+import Grid from "./Grid";
+import { Box, Typography } from "@material-ui/core";
+import { GameStates } from "./Constants";
+
+export default function GameView() {
+  const { currentGame, updateGame, currentPlayer } = useNavalBattleContext();
+  const enemyPlayer =
+    currentGame.player1.id === currentPlayer.id
+      ? currentGame.player2
+      : currentGame.player1;
+  console.log("Gamestate is");
+  console.log(currentGame.gameState);
+
+  if (
+    currentGame.gameState !== GameStates.PLAYER1WIN &&
+    currentGame.gameState !== GameStates.PLAYER2WIN
+  ) {
+    updateGame();
+  }
+
+  return (
+    <Box>
+      <h1>
+        The winner is :
+        <Typography variant="h3" component="h3" style={{ color: "#90F150" }}>
+          {currentGame.gameState === GameStates.PLAYER1WIN
+            ? " " + currentGame.player1.nickname
+            : " " + currentGame.player2.nickname}
+        </Typography>
+        !
+      </h1>
+      <Box style={{ display: "flex", flexDirection: "row", gridGap: "10px" }}>
+        <Box>
+          <Typography
+            variant="h3"
+            component="h3"
+            style={{ wordBreak: "break-all", color: "#5385FD" }}
+          >
+            {currentPlayer.nickname}'s grid :
+          </Typography>
+          <Grid cordinates={currentPlayer.grid.cordinates} />
+        </Box>
+        <Box>
+          <Typography
+            variant="h3"
+            component="h3"
+            style={{ wordBreak: "break-all", color: "#FF5733" }}
+          >
+            {enemyPlayer.nickname}'s grid :
+          </Typography>
+          <Grid cordinates={enemyPlayer.grid.cordinates} />
+        </Box>
+      </Box>
+    </Box>
+  );
+}
