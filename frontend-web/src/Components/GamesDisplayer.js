5cc68c784b5765d788f0335ff921c6b889438e5f
LuLu GiGi <lucamaggio98@hotmail.com>
2021-07-12 12:51:06 +0200

    add: creation of the component Chatter, add: installing SocketIO python, add: installing socket.io-client in React proj

---
+++ frontend-web/src/Components/GamesDisplayer.js
@@ -0,0 +1,118 @@
+import {
+  Box,
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Typography,
+} from "@material-ui/core";
+import Grid from "./Grid";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { useNavalBattleContext } from "../hooks/NavalBattleContextProvider";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: "100%",
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: "33.33%",
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+  accordionContent: {
+    display: "flex",
+    justifyContent: "space-between",
+    marginRight: "10px",
+  },
+  accordionSecondaryTitle: {
+    marginLeft: "10%",
+    marginRight: "10%",
+  },
+  accordionMainTitle: { marginRight: "10%" },
+}));
+
+export default function GamesDisplayer(games) {
+  const { currentPlayer } = useNavalBattleContext();
+  const styles = useStyles();
+
+  let listOfGames = [];
+
+  if (games !== null && games.lenght !== 0) {
+    games["games"].forEach((game) => {
+      console.log(game);
+      let gamePLayer =
+        currentPlayer.id === game["player1"].id
+          ? game["player1"]
+          : game["player2"];
+
+      let enemyPlayer =
+        currentPlayer.id === game["player1"].id
+          ? game["player2"]
+          : game["player1"];
+
+      let gamePlayerWon = currentPlayer.id === game["playerWinnerId"];
+
+      listOfGames.push(
+        <Accordion style={{ margin: "10px 0 10px 0" }}>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+            classes={{ content: styles.accordionContent }}
+          >
+            <Typography>{game["name"]}</Typography>
+            <Typography
+              style={{ color: gamePlayerWon ? "#90F150" : "#FF5733" }}
+            >
+              {gamePlayerWon ? "You Win this game" : "You Lost this game"}
+            </Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Box
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gridGap: "20px",
+              }}
+            >
+              <Box
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                }}
+              >
+                <Typography
+                  component="h2"
+                  style={{ wordBreak: "break-all", color: "#5385FD" }}
+                >
+                  {gamePLayer["nickname"]}
+                </Typography>
+                <Grid cordinates={gamePLayer["grid"]["cordinates"]} />
+              </Box>
+              <Box display="flex" flexDirection="column">
+                <Typography
+                  component="h2"
+                  style={{ wordBreak: "break-all", color: "#FF5733" }}
+                >
+                  {enemyPlayer["nickname"]}
+                </Typography>
+                <Grid cordinates={enemyPlayer["grid"]["cordinates"]} />
+              </Box>
+            </Box>
+          </AccordionDetails>
+        </Accordion>
+      );
+    });
+  }
+
+  return (
+    <Box>
+      <h1>Games displayer works!!</h1>
+      <Box>{listOfGames}</Box>
+    </Box>
+  );
+}
---
+++ frontend-web/src/Components/MessagesChat/Chatter.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect } from "react";
+import {Typography, Box, Button, Paper, TextField} from "@material-ui/core";
+import SendIcon from '@material-ui/icons/Send';
+
+import io from "socket.io-client";
+import { useNavalBattleContext } from "../../hooks/NavalBattleContextProvider";
+
+
+let endPoint = "http://localhost:5555";
+let socket = io.connect(`${endPoint}`);
+
+const Chatter = () => {
+  const { currentPlayer } = useNavalBattleContext();
+  const [messages, setMessages] = useState([""]);
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    getMessages();
+  }, [messages.length]);
+
+  const getMessages = () => {
+    socket.on("message", (msg) => {
+      //   let allMessages = messages;
+      //   allMessages.push(msg);
+      //   setMessages(allMessages);
+      setMessages([...messages, msg]);
+    });
+  };
+
+  // On Change
+  const onChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  // On Click
+  const onClick = () => {
+    if (message !== "") {
+      socket.emit("message", `${currentPlayer.nickname}: ${message}`);
+      // socket.emit("playerMessage", { "playerId": currentPlayer.id, "message":message });
+
+      setMessage("");
+    } else {
+      alert("Please Add A Message");
+    }
+  };
+
+  return (
+    <Paper elevation={3} style={{padding:"30px"}}>
+      <Typography component="h2">Welcome in the Chat!</Typography>
+      {messages.length > 0 &&
+        messages.map((msg) => (
+          <div>
+            <p>{msg}</p>
+          </div>
+        ))}
+      <Box display="flex" flexDirection="row" gridGap="15px" alignItems="center">
+        <TextField variant="outlined" value={message} name="message" onChange={(e) => onChange(e)} />
+        <Button style={{height:"40px"}} variant="contained" onClick={() => onClick()}  endIcon={<SendIcon/>}>Send Message </Button>
+      </Box>
+    </Paper>
+  );
+};
+
+export default Chatter;
